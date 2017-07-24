3be56809d5c113943e99bf49b541ae9edff4fff0
Galil Gertner <galilg@gmail.com>
2017-07-24 00:05:02 -0400

    Added Yelp business search.

--- lib/api/yelp/Yelp.py
+++ lib/api/yelp/Yelp.py
@@ -20,25 +20,10 @@ class Yelp(object):
         self.__ACCESS_TOKEN = None
         self.__load_access_token()
 
-
-    def __load_access_token(self):
-        try:
-            with open('access_token', 'r') as file:
-                date = file.readline().rstrip('\n')
-                token_expiration = datetime.datetime.strptime(date,
-                                                            '%Y-%m-%d %H:%M:%S')
-                now  = datetime.datetime.now()
-                if (now < token_expiration):
-                    self.__ACCESS_TOKEN = file.readline()
-
-                else:
-                    self.__get_new_token()
-        except:
-            self.__get_new_token()
-
+        self.__BASE_URL = 'https://api.yelp.com'
 
     def __get_new_token(self):
-        url = 'https://api.yelp.com/oauth2/token'
+        url = self.__BASE_URL + '/oauth2/token'
         params = {
             'grant_type': 'oauth2',
             'client_id': self.__CLIENT_ID,
@@ -62,4 +47,58 @@ class Yelp(object):
             file.write(expiration + '\n')
             file.write(content['access_token'])
 
+
+    def __load_access_token(self):
+        try:
+            with open('access_token', 'r') as file:
+                date = file.readline().rstrip('\n')
+                token_expiration = datetime.datetime.strptime(date,
+                                                            '%Y-%m-%d %H:%M:%S')
+                now  = datetime.datetime.now()
+                if (now < token_expiration):
+                    self.__ACCESS_TOKEN = file.readline()
+
+                else:
+                    self.__get_new_token()
+        except:
+            self.__get_new_token()
+
+
+    def __search_businesses(self, name, location=None, lat_long=None):
+        url = self.__BASE_URL + '/v3/businesses/search'
+
+        headers = {'Authorization': 'bearer %s' % self.__ACCESS_TOKEN}
+
+        if (location is None):
+            params = {
+                'term': name,
+                'latitude': lat_long[0],
+                'longitude': lat_long[1],
+                'radius': 5000
+            }
+        elif(lat_long is None):
+            params = {
+                'term': name,
+                'location': location
+            }
+
+        url += '?' + urllib.parse.urlencode(params)
+        request = requests.get(url=url, headers=headers)
+
+        #import pdb; pdb.set_trace()
+        if request.status_code != 200:
+            raise RuntimeError(
+                'the following request returned a status code of "{}" = "{}"'
+                    .format(request.status_code, url))
+
+        content = json.loads(request.content.decode())
+
+        return content
+
+
+    def search_location(self, name, location):
+        self.__search_businesses(name, location)
+
+    def search_lat_long(self, name, lat_long):
+        self.__search_businesses(name, None, lat_long)
 
--- lib/api/yelp/test_yelp.py
+++ lib/api/yelp/test_yelp.py
@@ -3,3 +3,5 @@
 from Yelp import Yelp
 
 yelp = Yelp()
+
+yelp.search_location('Bobo', 'New York, NY')
